ec98c225f2b93978ad19d59398461b4e8f13a197
Andrei Fridman <judoostudy@mail.ru>
2023-06-29 23:09:46 +0300

    Remove out

--- DB_HW_5/out/build/x64-Debug/libpqxx-build/test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/judoo/OneDrive/Рабочий стол/My_Projects_VS/Д.З. Базы данных/HomeWorkToSend/libpqxx-7.7.5/test
-# Build directory: C:/Users/judoo/OneDrive/Рабочий стол/My_Projects_VS/Д.З. Базы данных/HomeWorkToSend/DB_HW_5/out/build/x64-Debug/libpqxx-build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(runner "C:/Users/judoo/OneDrive/Рабочий стол/My_Projects_VS/Д.З. Базы данных/HomeWorkToSend/DB_HW_5/out/build/x64-Debug/libpqxx-build/test/runner.exe")
-set_tests_properties(runner PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/judoo/OneDrive/Рабочий стол/My_Projects_VS/Д.З. Базы данных/HomeWorkToSend/libpqxx-7.7.5/test/CMakeLists.txt;12;add_test;C:/Users/judoo/OneDrive/Рабочий стол/My_Projects_VS/Д.З. Базы данных/HomeWorkToSend/libpqxx-7.7.5/test/CMakeLists.txt;0;")
